b9cbc5dbedce4a8295373c0c80badae0bda331eb
rashimarasangi <rashimarasangi200@gmail.com>
2025-06-10 09:36:55 +0530

    cart section is added

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -3,9 +3,30 @@ import './PlaceOrder.css'
 
 const PlaceOrder = () => {
   return (
-    <div>
-      
-    </div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First Name' />
+          <input type="text" placeholder='Last Name'/>
+        </div>
+        <input type="email" placeholder='Emai address'/>
+        <input type="text" placeholder='Street'/>
+        <div className="multi-fields">
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State'/>
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zip code' />
+          <input type="text" placeholder='Country'/>
+        </div>
+        <input type="text" placeholder='Phone'/>
+      </div>
+      <div className="place-order-right">
+
+      </div>
+
+    </form>
   )
 }
 
