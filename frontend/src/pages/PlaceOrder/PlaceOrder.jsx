cb246125b94965ffb4cbc2489fb3ee58c986ca6c
rashimarasangi <rashimarasangi200@gmail.com>
2025-06-10 09:52:21 +0530

    place holder section is added

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
+
+  const {getTotalCartAmount} = useContext(StoreContext)
+
   return (
     <form className='place-order'>
       <div className="place-order-left">
@@ -23,6 +27,27 @@ const PlaceOrder = () => {
         <input type="text" placeholder='Phone'/>
       </div>
       <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+               <p>Subtotal</p>
+               <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+                <p>Delivery Fee</p>
+                <p>${2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+               <b>Total</b>
+               <b>${getTotalCartAmount()+2}</b>
+            </div>
+          </div>
+          <button >PROCEED TO PAYMENT</button>
+        </div>
+
 
       </div>
 
